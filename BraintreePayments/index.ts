d9821dfcb7dc274d038ccca989e18e7d8a1e64b1
Andrew Grischenko <andrew.grischenko@gmail.com>
2020-04-19 00:03:09 +0000

    Styling - font sizes and properties

--- BraintreePayments/index.ts
+++ BraintreePayments/index.ts
@@ -6,6 +6,8 @@ const STATUS_ERROR: string 		= "error";
 const STATUS_PROCESSING: string = "processing";
 const STATUS_COMPLETED: string 	= "completed";
 
+const DELAYED_INIT: number = 250; //ms
+
 export class BraintreePayments implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
 	private payment_status: string;
@@ -14,6 +16,11 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 	private payment_amount: number;
 	private dropin_ui: any;
 	private checkout_url: string;
+	private button_font_size: number;
+	private card_font_size: number;
+	private default_font_size: number;
+	private delayed_init_handler: any;
+	//private error_font_size: number;
 	/**
 	 * Empty constructor.
 	 */
@@ -63,6 +70,31 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 
 		if(this.checkout_url != context.parameters.CheckoutURL.raw && context.parameters.CheckoutURL.raw)
 			this.checkout_url = context.parameters.CheckoutURL.raw;
+
+		if(this.default_font_size != context.parameters.DefaultFontSize.raw)
+		{
+			this.default_font_size = context.parameters.DefaultFontSize.raw || 18;
+			document.documentElement.style.setProperty("--default-font-size", this.default_font_size + "pt"); 
+		}
+
+		if(this.card_font_size != context.parameters.CardFontSize.raw /*|| 
+		   this.error_font_size != context.parameters.ErrorFontSize.raw*/)
+		{
+			this.card_font_size = context.parameters.CardFontSize.raw || 20;
+			console.log("updating drop in, font size NOW=" + this.card_font_size + 'pt' );
+			//this.error_font_size = context.parameters.ErrorFontSize.raw || 20;
+			if(this.dropin_ui){
+				this.dropin_ui.teardown();
+				this.initBraintreeClient();
+			}
+		}
+		
+		if(this.button_font_size != context.parameters.ButtonFontSize.raw)
+		{
+			this.button_font_size = context.parameters.ButtonFontSize.raw || 22;
+			document.documentElement.style.setProperty("--button-font-size", this.button_font_size + "pt"); 
+		}
+		
 	}
 
 	/** 
@@ -88,83 +120,96 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 		if(!this.tokenization_key)
 			return;
 
-		const dropin = require('braintree-web-drop-in');
+		if(this.delayed_init_handler)
+			clearTimeout(this.delayed_init_handler);
 
-		try {
-			dropin.create({
-				authorization: this.tokenization_key,
-				container: '#dropin-container',
-				card: {
-					overrides: {
-					  styles: {
-						input: {
-						  'font-size': '18pt'
-						},
-						'.number': {
-						  // Custom web fonts are not supported. Only use system installed fonts.
-						},
-						'.invalid': {
-						  color: 'red'
-						}
-					  }
-					}
-				  }
-			},  (createErr: any, instance: any) => {
-				if(instance){	
-					this.dropin_ui = instance;	
-					this.setStatus(STATUS_NEW);
-					const submit_button = document.querySelector('#submit-button');
-					submit_button!.addEventListener('click', () => {
-						if(!this.checkout_url)
-							throw("No Checkout URL has been specified");
-						instance.requestPaymentMethod((err: any, payload: any) => {				
-							if(err || !payload){
-								this.errorProcessing(err);
-								return;
+		this.delayed_init_handler = setTimeout(() => {
+			this.delayed_init_handler = undefined;
+
+			const dropin = require('braintree-web-drop-in');
+
+			try {
+				console.log("craeting drop in, font size=" + this.card_font_size + 'pt' )
+				dropin.create({
+					authorization: this.tokenization_key,
+					container: '#dropin-container',
+					card: {
+						overrides: {
+						styles: {
+							input: {
+
+							},
+							'.number': {
+								'font-size': this.card_font_size + 'pt'
+							// Custom web fonts are not supported. Only use system installed fonts.
+							},
+							'.invalid': {
+							color: 'red'/*,
+							'font-size': this.error_font_size + 'pt'*/
 							}
-							
-							this.setStatus(STATUS_PROCESSING);
-
-							console.log('NONCE=' + payload.nonce);
-							fetch(this.checkout_url, {
-								method: 'POST',
-								headers : new Headers(),
-								body:JSON.stringify({
-									payment_method_nonce: payload.nonce,
-									amount: this.payment_amount
-								})
-							})
-							.then( (response:any) => {
-								if ((response.status >= 200 && response.status <= 299) || response.status == 500 ) {
-									return response.json();
-								  } else {
-									throw Error(response.statusText);
-								  }
-							})
-							.then( (data: any ) => {
-								if(data.success){
-									this.setStatus(STATUS_COMPLETED);
-									instance.teardown();
-								} else {
-									this.setStatus(STATUS_ERROR);
-									document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + data.message;
+						}
+						}
+					}
+				},  (createErr: any, instance: any) => {
+					if(instance){	
+						this.dropin_ui = instance;	
+						this.setStatus(STATUS_NEW);
+						const submit_button = document.querySelector('#submit-button');
+						submit_button!.addEventListener('click', () => {
+							if(!this.checkout_url)
+								throw("No Checkout URL has been specified");
+							instance.requestPaymentMethod((err: any, payload: any) => {				
+								if(err || !payload){
+									this.errorProcessing(err);
+									return;
 								}
-							})
-							.catch( (reason:any) => {
-								// Handle the error
-								this.setStatus(STATUS_ERROR);
-								document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + reason;
+								
+								this.setStatus(STATUS_PROCESSING);
+
+								console.log('NONCE=' + payload.nonce);
+								fetch(this.checkout_url, {
+									method: 'POST',
+									headers : new Headers(),
+									body:JSON.stringify({
+										payment_method_nonce: payload.nonce,
+										amount: this.payment_amount
+									})
+								})
+								.then( (response:any) => {
+									if ((response.status >= 200 && response.status <= 299) || response.status == 500 ) {
+										return response.json();
+									} else {
+										throw Error(response.statusText);
+									}
+								})
+								.then( (data: any ) => {
+									if(data.success){
+										this.setStatus(STATUS_COMPLETED);
+										instance.teardown();
+									} else {
+										this.setStatus(STATUS_ERROR);
+										document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + data.message;
+									}
+								})
+								.catch( (reason:any) => {
+									// Handle the error
+									this.setStatus(STATUS_ERROR);
+									document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + reason;
+								});
 							});
 						});
-					});
 
-				} else 
-					this.errorInitBraintreeClient(createErr);
-			});
-		}
-		catch (err) {
-			this.errorInitBraintreeClient(err);
-		}
+					} else 
+						this.errorInitBraintreeClient(createErr);
+				});
+			}
+			catch (err) {
+				this.errorInitBraintreeClient(err);
+			}
+		}, DELAYED_INIT);
+
+
+		
 	}
 
 	private errorInitBraintreeClient(errorMessage: string)
@@ -195,7 +240,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_NEW:
-					document.querySelector('#submit-button')?.classList.remove("button-hidden");
+					document.querySelector('#submit-button')?.classList.remove("hidden");
 					document.querySelector('#submit-button')?.removeAttribute("disabled");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.remove("hidden");
@@ -204,7 +249,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_PROCESSING:
-					document.querySelector('#submit-button')?.classList.add("button-hidden");
+					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
@@ -213,7 +258,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_COMPLETED:
-					document.querySelector('#submit-button')?.classList.add("button-hidden");
+					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
@@ -222,7 +267,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 					document.querySelector('#success')?.classList.remove("hidden");
 					break;
 				case STATUS_ERROR:
-					document.querySelector('#submit-button')?.classList.add("button-hidden");
+					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.remove("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
@@ -239,26 +284,28 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 	private getHTMLElements(): DocumentFragment 
 	{
 		let html = ` 
-			<div class="error hidden" id="error-div">
-				<span id="error-label">The connection to Braintree has not been initialised. Make sure you've specified correct TokenizationKey property value.</span>
-			</div>
-			<div id="dropin-container"></div>
-			<div id="processing" class="hidden">
-				<div class="processing">
-					<svg width="14" height="16" class="processing-lock">
-						<use xlink:href="#iconLockLoader"></use>
-					</svg>
+			<div class="control-container">
+				<div class="error hidden" id="error-div">
+					<span id="error-label">The connection to Braintree has not been initialised. Make sure you've specified correct TokenizationKey property value.</span>
 				</div>
-				<p>Processing...</p>
-			</div>
-			<button id="submit-button" class="submit-buton">Pay</button>
-			<div id="success" style="hidden">
-				<div class="success-icon">
-					<svg class="success-icon-tick">
-						<path class="path1" d="M14.379 29.76L39.741 3.415 36.194.001l-21.815 22.79-10.86-11.17L0 15.064z"></path>
-					</svg>				
+				<div id="dropin-container"></div>
+				<div id="processing" class="hidden">
+					<div class="processing">
+						<svg width="14" height="16" class="processing-lock">
+							<use xlink:href="#iconLockLoader"></use>
+						</svg>
+					</div>
+					<p>Processing...</p>
 				</div>
-				<p>The payment has been successfull.</p>
+				<button id="submit-button" class="hidden submit-buton">Pay</button>
+				<div id="success" class="hidden">
+					<div class="success-icon">
+						<svg class="success-icon-tick">
+							<path class="path1" d="M14.379 29.76L39.741 3.415 36.194.001l-21.815 22.79-10.86-11.17L0 15.064z"></path>
+						</svg>				
+					</div>
+					<p>The payment has been successfull.</p>
+				</div>
 			</div>`;
 		var template = document.createElement('template');
 		html = html.trim(); // Never return a text node of whitespace as the result
