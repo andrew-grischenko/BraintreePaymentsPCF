7fc6c57fd97f17d0f812dfd28ce4e3471daf7f43
Andrew Grischenko <andrew.grischenko@gmail.com>
2020-04-26 06:25:52 +0000

    PayPal payment method, styling fixes and refactor

--- BraintreePayments/index.ts
+++ BraintreePayments/index.ts
@@ -21,6 +21,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 	private default_font_size: number;
 	private delayed_init_handler: any;
 	private has_been_reset: boolean;
+	private paypal_enabled: boolean;
 	//private error_font_size: number;
 	/**
 	 * Empty constructor.
@@ -55,19 +56,28 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 	 */
 	public updateView(context: ComponentFramework.Context<IInputs>): void
 	{
+		enum BraintreeResetRequired {
+			NO,
+			YES_NEW,
+			YES_UNINITIALISED
+		};
+
+		let isBraintreeResetRequired = BraintreeResetRequired.NO;
+
 		if(this.tokenization_key != context.parameters.TokenizationKey.raw)
 		{
-			this.cleanupBraintreeClient(true);
-
 			this.tokenization_key = context.parameters.TokenizationKey.raw || "";
+			isBraintreeResetRequired = BraintreeResetRequired.YES_UNINITIALISED;
+		}
 
-			this.initBraintreeClient();
+		if(this.paypal_enabled != context.parameters.PayPalEnabled.raw){
+			this.paypal_enabled = context.parameters.PayPalEnabled.raw;
+			isBraintreeResetRequired = BraintreeResetRequired.YES_NEW;	
 		}
 
 		if(context.parameters.Reset.raw && !this.has_been_reset ){
 			this.has_been_reset = true;
-			this.cleanupBraintreeClient(false);
-			this.initBraintreeClient();
+			isBraintreeResetRequired = BraintreeResetRequired.YES_NEW;	
 		} else
 			this.has_been_reset = false;
 
@@ -88,9 +98,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 		{
 			this.card_font_size = context.parameters.CardFontSize.raw || 20;
 			//this.error_font_size = context.parameters.ErrorFontSize.raw || 20;
-			this.cleanupBraintreeClient(false);
-			this.initBraintreeClient();
-			
+			isBraintreeResetRequired = BraintreeResetRequired.YES_NEW;	
 		}
 		
 		if(this.button_font_size != context.parameters.ButtonFontSize.raw)
@@ -98,7 +106,11 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 			this.button_font_size = context.parameters.ButtonFontSize.raw || 22;
 			document.documentElement.style.setProperty("--button-font-size", this.button_font_size + "pt"); 
 		}
-		
+
+		if(isBraintreeResetRequired != BraintreeResetRequired.NO){
+			this.cleanupBraintreeClient(isBraintreeResetRequired == BraintreeResetRequired.YES_UNINITIALISED);
+			this.initBraintreeClient();			
+		}
 	}
 
 	/** 
@@ -126,6 +138,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 		this.dropin_ui = undefined;
 		if(isThroughUnititialised)
 			this.setStatus(STATUS_UNINITIALISED);
+		document.querySelector('#submit-button')?.remove();
 	}
 
 	private initBraintreeClient()
@@ -141,80 +154,134 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 
 			const dropin = require('braintree-web-drop-in');
 
-			try {
-				console.log("craeting drop in, font size=" + this.card_font_size + 'pt' )
-				dropin.create({
-					authorization: this.tokenization_key,
-					container: '#dropin-container',
-					card: {
-						overrides: {
-						styles: {
-							input: {
-								'font-size': this.card_font_size + 'pt'
-							},
-							'.number': {
+			const styles =  {
+				input: {
+					'font-size': this.card_font_size + 'pt'
+				},
+				'#card-sheet-header': {
+					'font-size': this.card_font_size + 'pt'
+				},
+				'.number': {
 
-							// Custom web fonts are not supported. Only use system installed fonts.
-							},
-							'.invalid': {
-							color: 'red'/*,
-							'font-size': this.error_font_size + 'pt'*/
-							}
+				// Custom web fonts are not supported. Only use system installed fonts.
+				},
+				'.invalid': {
+				color: 'red'/*,
+				'font-size': this.error_font_size + 'pt'*/
+				}
+			};
+
+			const options = (this.paypal_enabled) ? 
+			{
+				authorization: this.tokenization_key,
+				container: '#dropin-container',
+				card: {
+					overrides: {
+						styles: styles
 						}
+				},
+				paypal: {
+					flow: 'vault'
+				}
+			} :
+			{
+				authorization: this.tokenization_key,
+				container: '#dropin-container',
+				card: {
+					overrides: {
+						styles: styles
 						}
-					}
-				},  (createErr: any, instance: any) => {
+				}
+			};
+
+			try {
+
+				dropin.create(options,  (createErr: any, instance: any) => {
 					if(instance){	
 						this.dropin_ui = instance;	
 						this.setStatus(STATUS_NEW);
-						const submit_button = document.querySelector('#submit-button');
-						submit_button!.addEventListener('click', () => {
+						const submit_button = document.createElement("button");
+						submit_button.setAttribute("id", "submit-button");
+						submit_button.classList.add("submit-button");
+						submit_button.textContent = "Pay";
+						document.querySelector(".control-container")!.append(submit_button);
+						this.setSubmitButtonState(false);
+
+						submit_button!.addEventListener('click', ()=> { 
 							if(!this.checkout_url)
 								throw("No Checkout URL has been specified");
-							instance.requestPaymentMethod((err: any, payload: any) => {				
-								if(err || !payload){
-									this.errorProcessing(err);
-									return;
-								}
-								
-								this.setStatus(STATUS_PROCESSING);
 
-								console.log('NONCE=' + payload.nonce);
-								fetch(this.checkout_url, {
-									method: 'POST',
-									headers : new Headers(),
-									body:JSON.stringify({
-										payment_method_nonce: payload.nonce,
-										amount: this.payment_amount
-									})
+							this.dropin_ui.requestPaymentMethod((err: any, payload: any) => {				
+							if(err || !payload){
+								this.errorProcessing(err);
+								return;
+							}
+							
+							this.setStatus(STATUS_PROCESSING);
+
+							console.log('NONCE=' + payload.nonce);
+							fetch(this.checkout_url, {
+								method: 'POST',
+								headers : new Headers(),
+								body:JSON.stringify({
+									payment_method_nonce: payload.nonce,
+									amount: this.payment_amount
 								})
-								.then( (response:any) => {
-									if ((response.status >= 200 && response.status <= 299) || response.status == 500 ) {
-										return response.json();
-									} else {
-										throw Error(response.statusText);
-									}
-								})
-								.then( (data: any ) => {
-									if(data.success){
-										this.setStatus(STATUS_COMPLETED);
-										this.cleanupBraintreeClient(false);
-									} else {
-										this.setStatus(STATUS_ERROR);
-										document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + data.message;
-									}
-								})
-								.catch( (reason:any) => {
-									// Handle the error
+							})
+							.then( (response:any) => {
+								if ((response.status >= 200 && response.status <= 299) || response.status == 500 ) {
+									return response.json();
+								} else {
+									throw Error(response.statusText);
+								}
+							})
+							.then( (data: any ) => {
+								if(data.success){
+									this.setStatus(STATUS_COMPLETED);
+									this.cleanupBraintreeClient(false);
+								} else {
 									this.setStatus(STATUS_ERROR);
-									document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + reason;
-								});
+									document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + data.message;
+								}
+							})
+							.catch( (reason:any) => {
+								// Handle the error
+								this.setStatus(STATUS_ERROR);
+								document.querySelector('#error-label')!.textContent = "There's been an error processing payment: " + reason;
 							});
+						});
+						});
+
+						const dropin_toggle = document.querySelector('[data-braintree-id="toggle"]');
+						dropin_toggle!.addEventListener('click', () => {
+							if (!instance.isPaymentMethodRequestable())
+								this.setSubmitButtonState(false);
+						});
+						
+						if(!this.paypal_enabled)
+							this.setSubmitButtonState(true, false);
+						
+						instance.on('paymentOptionSelected', (event: any) => {
+							if(event.paymentOption == 'card')
+								this.setSubmitButtonState(this.payment_status != STATUS_PROCESSING, instance.isPaymentMethodRequestable());
+							else
+								this.setSubmitButtonState(false);
+						});
+
+						instance.on('paymentMethodRequestable', (event: any) => {
+							this.setSubmitButtonState(this.payment_status != STATUS_PROCESSING, true);
+						});
+							
+						instance.on('noPaymentMethodRequestable',  () => {
+							if(instance.paymentOptionSelected == "card")
+								this.setSubmitButtonState(true, false);
 						});
 
 					} else 
 						this.errorInitBraintreeClient(createErr);
 				});
+
+				  
 			}
 			catch (err) {
 				this.errorInitBraintreeClient(err);
@@ -231,7 +298,7 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 
 	private errorProcessing(errorMessage: string)
 	{
-		//this.setStatus(STATUS_ERROR);
+		this.setStatus(STATUS_ERROR);
 		document.querySelector('#error-label')!.textContent = "There's been an error processing the payments: " + errorMessage;
 	}
 
@@ -241,55 +308,59 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 		{
 			switch(status){
 				case STATUS_UNINITIALISED:
-					document.querySelector('#submit-button')?.classList.remove("hidden");
-					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-label')!.textContent="The connection to Braintree has not been initialised. Make sure you've specified correct TokenizationKey property value.";
 					document.querySelector('#error-div')?.classList.remove("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
 					document.querySelector('#processing')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_NEW:
-					document.querySelector('#submit-button')?.classList.remove("hidden");
-					document.querySelector('#submit-button')?.removeAttribute("disabled");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.remove("hidden");
 					document.querySelector('#processing')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.classList.remove("hidden");
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_PROCESSING:
-					document.querySelector('#submit-button')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
 					document.querySelector('#processing')?.classList.remove("hidden");
-					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 				case STATUS_COMPLETED:
-					document.querySelector('#submit-button')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.add("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
 					document.querySelector('#processing')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#success')?.classList.remove("hidden");
 					break;
 				case STATUS_ERROR:
-					document.querySelector('#submit-button')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.setAttribute("disabled", "true");
 					document.querySelector('#error-div')?.classList.remove("hidden");
 					document.querySelector('#dropin-container')?.classList.add("hidden");
 					document.querySelector('#processing')?.classList.add("hidden");
-					document.querySelector('#submit-button')?.classList.add("hidden");
 					document.querySelector('#success')?.classList.add("hidden");
 					break;
 			}
+
+			this.setSubmitButtonState(false);
 			this.payment_status = status;
 			this._notifyOutputChanged();
 		}
+	}
+
+	private setSubmitButtonState(isVisible: boolean, isEnabled: boolean = false){
+		const submitButton = document.querySelector('#submit-button');
+
+		if(!submitButton)
+			return;
+
+		if(isVisible)
+			submitButton.classList.remove("hidden");
+		else
+			submitButton.classList.add("hidden");
+
+		if(isEnabled)
+			submitButton.removeAttribute("disabled");
+		else
+			submitButton.setAttribute("disabled", "true");
 	}
 
 	private getHTMLElements(): DocumentFragment 
@@ -308,7 +379,6 @@ export class BraintreePayments implements ComponentFramework.StandardControl<IIn
 					</div>
 					<p>Processing...</p>
 				</div>
-				<button id="submit-button" class="hidden submit-buton">Pay</button>
 				<div id="success" class="hidden">
 					<div class="success-icon">
 						<svg class="success-icon-tick">
